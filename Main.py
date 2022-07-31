a9af80b1de3fc05e05f23377932d4d5ee448620d
Alan Babu <97074137+Alan7149@users.noreply.github.com>
2022-07-31 10:18:23 +0530

    fifth commit

--- Main.py
+++ Main.py
@@ -24,14 +24,12 @@ class Evaluate:
     Returns:
       True if it is empty, else returns False.
     """
-      # Write your code here
-     if self.top == -1:
-       return True
-     else:
-       return False
-
-
-
+    # Write your code here
+    if self.top == -1:
+      return True
+    else:
+      return False
+    
   def pop(self):
     """
     Do pop operation if the stack is not empty.
